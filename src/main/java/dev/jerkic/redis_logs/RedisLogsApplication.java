393d8c24ce669de91a8702b631d2c3b5aeb2be88
Marko Jerkić <markojerkic266@gmail.com>
2025-11-29 07:59:49 +0100

    init

---
+++ src/main/java/dev/jerkic/redis_logs/RedisLogsApplication.java
@@ -0,0 +1,13 @@
+package dev.jerkic.redis_logs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisLogsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisLogsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/jerkic/redis_logs/RedisLogsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jerkic.redis_logs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisLogsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
